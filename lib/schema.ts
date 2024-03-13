7541f191b981c514d5d897dcd6796725e46d95a1
braden <26287414+bradenschiller@users.noreply.github.com>
2024-03-12 20:51:19 -0500

    Add db using drizzle

---
+++ lib/schema.ts
@@ -0,0 +1,12 @@
+import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+
+export const user = pgTable("issue", {
+  id: serial("id").primaryKey(),
+  title: text("title"),
+  description: text("description"),
+  status: text("status", { enum: ["OPEN", "IN_PROGRESS", "CLOSED"] }).default(
+    "OPEN"
+  ),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at"),
+});
